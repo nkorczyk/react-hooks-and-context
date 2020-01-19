a8d1911269cf2231abaa4f392c71ce88319aff36
Norbert Korczyk <korczyk.norbert@gmail.com>
2020-01-19 19:43:32 +0100

    Creating Context with Functional Components.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import React from 'react';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
 import ThemeContextProvider from './contexts/ThemeContext';
-import ThemeToggle from './components/ThemeToggle';
 import AuthContextProvider from './contexts/AuthContext';
+import BookContextProvider from './contexts/BookContext';
+import ThemeToggle from './components/ThemeToggle';
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
       <ThemeContextProvider>
         <AuthContextProvider>
           <Navbar />
-          <BookList />
+          <BookContextProvider>
+            <BookList />
+          </BookContextProvider>
           <ThemeToggle />
         </AuthContextProvider>
       </ThemeContextProvider>
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 
 import { ThemeContext } from '../contexts/ThemeContext';
+import { BookContext } from '../contexts/BookContext';
 
 // class BookList extends Component {
 //   static contextType = ThemeContext;
@@ -24,15 +25,21 @@ import { ThemeContext } from '../contexts/ThemeContext';
 // using useContext hook
 const BookList = () => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
 
   const theme = isLightTheme ? light : dark;
-
+  console.log(books);
   return (
     <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
       <ul>
-        <li style={{ background: theme.ui }}>the way of kings</li>
-        <li style={{ background: theme.ui }}>the name of the wind</li>
-        <li style={{ background: theme.ui }}>the final empire</li>
+        {/*<li style={{ background: theme.ui }}>the way of kings</li>*/}
+        {/*<li style={{ background: theme.ui }}>the name of the wind</li>*/}
+        {/*<li style={{ background: theme.ui }}>the final empire</li>*/}
+        {books.map(book => (
+          <li key={book.id} style={{ background: theme.ui }}>
+            {book.title}
+          </li>
+        ))}
       </ul>
     </div>
   );
